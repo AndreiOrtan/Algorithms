5f90ada7a722b27f17ff83d99dfe660158cae7ed
ortanandrei <ortanandrei2000@gmail.com>
2022-09-01 15:25:46 +0300

    Solved reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // My attempt
+  let combined = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    combined += str[i];
+  }
+  return combined;
+
+  // 2nd
+  // const team = [...str];
+  // team.reverse();
+  // return team.join("");
+
+  //3rd
+  // return str.split("").reduce((firstValue, lastValue) => {
+  //   return lastValue + firstValue;
+  // });
+}
 
 module.exports = reverse;
