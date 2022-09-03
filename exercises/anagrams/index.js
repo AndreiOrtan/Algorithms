879742c8b51f8b4b1364416e240cac205b41f718
ortanandrei <ortanandrei2000@gmail.com>
2022-09-03 23:02:15 +0300

    solved anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,39 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(str1, str2) {
+  //   const word1 = buildMap(str1);
+  //   const word2 = buildMap(str2);
+  //   if (Object.keys(word1).length !== Object.keys(word2).length) {
+  //     return false;
+  //   }
+  //   for (const key in word1) {
+  //     if (word1[key] !== word2[key]) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // }
+  // function buildMap(str) {
+  //   const chars = {};
+  //   for (const char of str.replace(/[^\w]/g, "").toLowerCase()) {
+  //     if (!chars[char]) {
+  //       chars[char] = 1;
+  //     } else {
+  //       chars[char] += 1;
+  //     }
+  //   }
+  //   return chars;
+
+  //
+
+  const word1 = sortString(str1);
+  const word2 = sortString(str2);
+  return word1 === word2;
+
+  function sortString(str) {
+    return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+  }
+}
 
 module.exports = anagrams;
