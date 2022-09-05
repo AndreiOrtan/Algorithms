78e840a9da57641ee36096d3e7d0b7602b7ab279
ortanandrei <ortanandrei2000@gmail.com>
2022-09-05 16:25:29 +0300

    volews

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,20 +14,37 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  const midpoint = Math.floor((2 * n - 1) / 2);
-  for (let row = 0; row < n; row++) {
-    let pyr = "";
-    for (let col = 0; col < 2 * n - 1; col++) {
-      if (midpoint - row <= col && midpoint + row >= col) {
-        pyr += "#";
-      } else {
-        pyr += " ";
-      }
-    }
+function pyramid(n, row = 0, pyr = "") {
+  //   const midpoint = Math.floor((2 * n - 1) / 2);
+  //   for (let row = 0; row < n; row++) {
+  //     let pyr = "";
+  //     for (let col = 0; col < 2 * n - 1; col++) {
+  //       if (midpoint - row <= col && midpoint + row >= col) {
+  //         pyr += "#";
+  //       } else {
+  //         pyr += " ";
+  //       }
+  //     }
 
-    console.log(pyr);
+  //     console.log(pyr);
+  //   }
+
+  //recursive method
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  if (n === row) {
+    return;
   }
+  if (pyr.length === 2 * n - 1) {
+    console.log(pyr);
+    return pyramid(n, row + 1);
+  }
+  if (midpoint - row <= pyr.length && midpoint + row >= pyr.length) {
+    pyr += "#";
+  } else {
+    pyr += " ";
+  }
+
+  pyramid(n, row, pyr);
 }
 
 module.exports = pyramid;
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,29 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  //   let vowels = "";
+  //   let word = str.toLowerCase();
+  //   for (let i = 0; i <= word.length; i++) {
+  //     if (
+  //       word[i] === "a" ||
+  //       word[i] === "e" ||
+  //       word[i] === "i" ||
+  //       word[i] === "o" ||
+  //       word[i] === "u"
+  //     ) {
+  //       vowels += word[i];
+  //     }
+  //   }
+  //   return vowels.length;
+  let count = 0;
+  const letters = ["a", "e", "i", "o", "u"];
+  for (const char of str.toLowerCase()) {
+    if (letters.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = vowels;
